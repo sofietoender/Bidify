43cea7333500565b52bab77d6bbbf2e12d9f9aaf
Sofie Tønder <sofie.ts@hotmail.com>
2024-12-30 21:49:36 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./**/*.{html,js}", "!./node_modules/**/*"],
 	theme: {
-		extend: {},
+		colors: {
+			beige: "#F6F4F0",
+			red: "#D6482C",
+			black: "#161613",
+			brown: "#A58063",
+			gray: "#AFA9A4",
+		},
 	},
-	plugins: [],
 };
