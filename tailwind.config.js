6ebdd086cfec8c6796d02a4bf2fbdaaec062cb81
Sofie Tønder <sofie.ts@hotmail.com>
2024-12-31 10:32:57 +0100

    added html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	theme: {
-		colors: {
-			beige: "#F6F4F0",
-			red: "#D6482C",
-			black: "#161613",
-			brown: "#A58063",
-			gray: "#AFA9A4",
-		},
-	},
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        beige: "#F6F4F0",
+        red: "#D6482C",
+        customblack: "#161613",
+        brown: "#A58063",
+        gray: "#AFA9A4",
+      },
+    },
+  },
+  plugins: [],
 };
